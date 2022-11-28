1d13b6304ddb6d26cb6ff1fe5f62eff2d6a289c8
Mihai1389 <99672988+Mihai1389@users.noreply.github.com>
2022-11-28 11:28:22 +0200

    Calc

--- src/App.js
+++ src/App.js
@@ -37,19 +37,19 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <OperationDigitButton operation="÷" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <OperationDigitButton operation="*" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <OperationDigitButton operation="+" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <OperationDigitButton operation="-" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,9 +1,9 @@
-import { ACTIONS } from "./App";
+import App, { ACTIONS } from "./App";
 export default function OperationButton({ dispatch, operation }) {
   return (
     <button
       onclick={() =>
-        dispatch({ type: ACTIONS.CHOOSE_ADD_DIGIT, payload: { operation } })
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
       }
     >
       {operation}
